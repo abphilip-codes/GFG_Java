9936fe03af775edd7aeb7cbfbb1ac7f8f9b34909
allenalvin333 <allenalvin333@gmail.com>
2021-07-27 13:44:18 +0530

    programs

---
+++ M1/Test.java
@@ -0,0 +1,16 @@
+package M1;
+class Student
+{
+    public static void main(String[] args)   
+    {
+        System.out.println("Class File Structure 1");
+    }
+}
+// Class Declaration
+class Test
+{
+       public static void main(String[] args)   
+       {
+           System.out.println("Class File Structure 2");
+       }
+} 
