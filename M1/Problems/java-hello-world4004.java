349206a977f626aa892c144742444e8fd53d775c
allenalvin333 <allenalvin333@gmail.com>
2021-08-05 09:00:06 +0530

    programs

---
+++ M1/Problems/java-hello-world4004.java
@@ -0,0 +1,20 @@
+// { Driver Code Starts
+//Initial Template for Java
+package Problems;
+
+class GFG9
+{
+    public static void main(String args[])
+    {
+        Solution9 ob = new Solution9();
+        ob.printHelloWorld();
+    }
+}
+// } Driver Code Ends
+
+
+class Solution9 {
+    void printHelloWorld(){
+        System.out.println("Hello World");
+    }
+}
