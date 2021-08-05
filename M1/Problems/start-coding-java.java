a278071c9433c6582be82f4c3da0b00a731153e3
allenalvin333 <allenalvin333@gmail.com>
2021-08-05 08:22:24 +0530

    programs

---
+++ M1/Problems/start-coding-java.java
@@ -0,0 +1,34 @@
+// https://practice.geeksforgeeks.org/problems/start-coding-java
+
+// { Driver Code Starts
+//Initial Template for Java
+
+ // } Driver Code Ends
+//User function Template for Java
+
+package Problems;
+class Geeks{
+    
+    // Function to print hello
+    void printHello(){
+        
+        // Your code here
+        System.out.print("Hello World");
+    }
+    
+}
+
+// { Driver Code Starts.
+
+class GfG{
+    
+    public static void main(String args[]){
+       
+       //Creating an Object of Class Geeks
+        Geeks g = new Geeks();
+        
+        //Calling printHello() function of the Class Geeks
+        g.printHello();
+    }
+    
+}  // } Driver Code Ends
