21429810e1b710d2c0e126dd34d437def77e65fd
allenalvin333 <allenalvin333@gmail.com>
2021-08-10 07:19:35 +0530

    programs

---
+++ M2/Lecture/BrianKernighan.java
@@ -0,0 +1,21 @@
+package Lecture;
+import java.io.*;
+
+class countSetBits {
+    static int count(int n)
+    {
+        int count = 0;
+        while (n > 0) {
+            n &= (n - 1);
+            count++;
+        }
+        return count;
+    }
+
+    // driver program
+    public static void main(String args[])
+    {
+        int i = 9;
+        System.out.println(count(i));
+    }
+}
