44880d6923512b3775f361925a6d5e4b0322589d
allenalvin333 <allenalvin333@gmail.com>
2021-08-13 06:53:14 +0530

    programs

--- M2/Problems/need-some-change-2.java
+++ M2/Problems/need-some-change-2.java
@@ -3,28 +3,28 @@
 
 package Problems;
 import java.util.*;
-import java.lang.*;
 class GFG6
 {
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args)
     {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
+        Scanner br = new Scanner(System.in);
+        int T = Integer.parseInt(br.nextLine().trim());
         while(T-->0)
         {
-            int n = Integer.parseInt(br.readLine().trim());
-            String[] S = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(br.nextLine().trim());
+            String[] S = br.nextLine().trim().split(" ");
             int[] arr = new int[n];
             for(int i = 0; i < n; i++){
                 arr[i] = Integer.parseInt(S[i]);
             }
-            Solution obj = new Solution();
+            Solution6 obj = new Solution6();
             obj.swapElements(arr, n);
             for(int i = 0; i < n; i++){
                 System.out.print(arr[i] + " ");
             }
             System.out.println();
         }
+        br.close();
     }
 }
 // } Driver Code Ends
